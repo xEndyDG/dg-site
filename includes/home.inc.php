93bada8fd6e70efcd77a192b581a653c297bf1d3
maoro <maoro@booomdigital.nl>
2022-04-04 10:31:31 +0200

    innitial push

---
+++ includes/home.inc.php
@@ -0,0 +1,6 @@
+<section class="home">
+    <div class="home container">
+        <h1>Dg website</h1>
+        <h3>Coming soon</h3>
+    </div>
+</section>
---
+++ index.php
@@ -0,0 +1,38 @@
+<?php
+session_start();    //zet sessie in alle pagina's
+if (isset($_GET["page"])) { //als de GET 'page' niet leeg is
+    if ($_GET['page'] == 'logout') {    //als de GET 'page' gelijk is aan uitloggen
+        header('Location: php/logout.php'); //ga naar
+        exit();
+    } else {    //anders ga naar $page
+        $page = $_GET['page'];
+    }
+} else {    //anders ga naar home
+    $page = 'home';
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Index</title>
+    <!--    link naar stylesheet-->
+    <link rel="stylesheet" href="css/style.css">
+    <!--    link naar javascript-->
+</head>
+
+
+<body>
+<div class="container">
+    <div class="content">
+<!--        --><?php //include "includes/navigation.inc.php"; ?>
+        <?php include "includes/$page.inc.php"; ?>
+    </div>
+    <div class="footer">
+        <?php include "includes/footer.inc.php"; ?>
+    </div>
+</div>
+</body>
+</html>
+
